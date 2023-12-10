cc9e123ea6f4a5e16482b45a5a50f4e83b7f2ad5
zaid03 <terguyzaid@gmail.com>
2023-12-10 13:04:14 +0100

    fix console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -2,100 +2,55 @@
 """Module for testing the HBNBCommand Class"""
 import unittest
 from console import HBNBCommand
-from unittest.mock import patch
-from io import StringIO
 
-
-class Test_Console(unittest.TestCase):
-    """Test the HBNBCommand Console"""
-
-#     def test_help(self):
-#         """Tests the help commmand"""
-#         with patch('sys.stdout', new=StringIO()) as f:
-#             HBNBCommand().onecmd("help")
-#         string = """
-# Documented commands (type help <topic>):
-# ========================================
-# EOF  all  count  create  destroy  help  quit  show  update
-# """
-#         msg = f.getvalue()
-#         self.assertEqual(string, msg)
+class TestConsole(unittest.TestCase):
+    """Test case class for testing the HBNBCommand Console."""
 
     def test_help(self):
         """Tests the help command."""
-        with patch('sys.stdout', new=StringIO()) as f:
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("help")
-        s = """
+        expected_output = """
 Documented commands (type help <topic>):
 ========================================
 EOF  all  count  create  destroy  help  quit  show  update\n
 """
-        self.assertEqual(s, f.getvalue())
-
-    # Test cases for quit
+        self.assertEqual(expected_output, log.output[0])
 
     def test_do_quit(self):
-        """Tests the quit commmand"""
-        with patch('sys.stdout', new=StringIO()) as f:
+        """Tests the quit command."""
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("quit")
-        # modelling what happens when someone types `quit`
-        msg = f.getvalue()
-        self.assertTrue(len(msg) == 0)
-        self.assertEqual("", msg)
+        self.assertEqual([], log.output)
 
-        with patch('sys.stdout', new=StringIO()) as f:
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("quit garbage")
-        # modelling when user types `quit anything`
-        msg = f.getvalue()
-        self.assertTrue(len(msg) == 0)
-        self.assertEqual("", msg)
+        self.assertEqual([], log.output)
 
-    # Test cases for EOF
     def test_do_EOF(self):
-        """Tests the EOF commmand"""
-        with patch('sys.stdout', new=StringIO()) as f:
+        """Tests the EOF command."""
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("EOF")
-        # modelling what happens when user types `quit`
-        msg = f.getvalue()
-        self.assertTrue(len(msg) == 1)
-        self.assertEqual("\n", msg)
+        self.assertEqual(['\n'], log.output)
 
-        with patch('sys.stdout', new=StringIO()) as f:
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("EOF garbage")
-        # modelling when user types `EOF anything`
-        msg = f.getvalue()
-        self.assertTrue(len(msg) == 1)
-        self.assertEqual("\n", msg)
+        self.assertEqual(['\n'], log.output)
 
-    # Test cases for emptyline
     def test_do_emptyline(self):
-        """Tests the emptyline command"""
-        with patch('sys.stdout', new=StringIO()) as f:
+        """Tests the emptyline command."""
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("\n")
-        # modelling what happens when user doesn't type anything
-        msg = f.getvalue()
-        self.assertTrue(len(msg) == 0)
-        self.assertEqual("", msg)
+        self.assertEqual([], log.output)
 
-        with patch('sys.stdout', new=StringIO()) as f:
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("                     \n")
-        # modelling when user types lots of whitespaces & enter
-        msg = f.getvalue()
-        self.assertTrue(len(msg) == 0)
-        self.assertEqual("", msg)
+        self.assertEqual([], log.output)
 
-    # Test cases for do_all
     def test_do_all(self):
-        """Tests the do_all command"""
-        with patch('sys.stdout', new=StringIO()) as f:
+        """Tests the do_all command."""
+        with self.assertLogs() as log:
             HBNBCommand().onecmd("all")
-
-    # Test cases for do_count
-    # Test cases for do_show
-    # Test cases for do_create
-    # Test cases for do_update
-    # Test cases for do_destroy
-
 
 if __name__ == "__main__":
     unittest.main()
